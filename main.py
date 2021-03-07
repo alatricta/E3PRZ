e91324e87e8662c8fed76ab5ecbe08605db1af20
alatricta <freelancer007@list.ru>
2021-03-07 19:38:13 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import win32com.client as wcom
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    app = wcom.Dispatch('CT.Application')
+    job = app.CreateJobObject()
+    sht = job.CreateSheetObject()
+
+    shts=job.GetAllSheetIds()
+#    print(shts[1][1])
+    for id in shts[1][1:]:
+        print(id)
+#    app.PutMessage("Привет от Python!")
