f33b977ca0d70c8484dc158f05277a88a32fd0e7
alatricta <freelancer007@list.ru>
2021-03-09 04:15:14 +0300

    Получение списка ID блоков

--- main.py
+++ main.py
@@ -10,9 +10,23 @@ if __name__ == '__main__':
     app = wcom.Dispatch('CT.Application')
     job = app.CreateJobObject()
     sht = job.CreateSheetObject()
+    sym = job.CreateSymbolObject()
 
-    shts = job.GetTreeSelectedSheetIds()
-#    for id in shts[1][1:]:
-#        print(id)
-#    app.PutMessage("Привет от Python!")
+    shts = job.GetTreeSelectedSheetIds()    #Получение списка ID выделенных листов
+#    shts = job.GetActiveSheetId()    #Получение ID активного окна
+
+    if shts[0] > 0:
+        for id in shts[1][1:]:
+            #Здесь надо получить список символов блоков со всех листов и скинуть всё в один список
+            sht.SetId(id)
+            sym_ids = sht.GetSymbolIds()
+            if sym_ids[0] > 0:
+                block_ids = []  # Создал пустой список ID блоков
+                for sym_id in sym_ids[1][1:]:
+                    sym.SetId(sym_id)
+                    if sym.IsBlock():
+                        block_ids.append(sym_id)
+
+    print(block_ids)
+    print('Количество блоков на листе:', len(block_ids))
 
