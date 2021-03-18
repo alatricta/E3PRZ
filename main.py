be42442443e031a01bfc971f3de89febc2b75fbe
alatricta <freelancer007@list.ru>
2021-03-18 21:40:43 +0300

    Получаю список символов на выделенных листах, и записываю всё в словарь

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import win32com.client as wcom
-
+    
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     app = wcom.Dispatch('CT.Application')
@@ -12,21 +12,35 @@ if __name__ == '__main__':
     sht = job.CreateSheetObject()
     sym = job.CreateSymbolObject()
 
-    shts = job.GetTreeSelectedSheetIds()    #Получение списка ID выделенных листов
-#    shts = job.GetActiveSheetId()    #Получение ID активного окна
+    # Получение количества и списка ID выделенных листов
+    shts = job.GetTreeSelectedSheetIds()
 
     if shts[0] > 0:
-        for id in shts[1][1:]:
-            #Здесь надо получить список символов блоков со всех листов и скинуть всё в один список
-            sht.SetId(id)
-            sym_ids = sht.GetSymbolIds()
+        # TODO: получить словарь {id_символа : ( (Х_мин, Y_мин, Х_макс, Y_макс), id_сборки)}
+        '''
+        id_символа - id символа блока на листе
+        Х_мин - координата левого края блока
+        Y_мин - координата нижнего края блока
+        Х_макс - координата правого края блока
+        Y_макс - координата верхнего края блока   
+        id_сборки - id сборки, в которую входит блок. Если блок в сборку не входит, то 0
+        devices - описанный словарь
+        coord - список координат, который будет внутри словаря
+        '''
+        devices = {}
+
+        for sht_id in shts[1][1:]:
+            sht.SetId(sht_id)
+            sym_ids = sht.GetSymbolIds()    # Получаем количество и список id всех символов на листе
             if sym_ids[0] > 0:
-                block_ids = []  # Создал пустой список ID блоков
                 for sym_id in sym_ids[1][1:]:
                     sym.SetId(sym_id)
+                    # вносим в словарь только символы блоков 
                     if sym.IsBlock():
-                        block_ids.append(sym_id)
+                        coord = sym.GetArea()
+                        devices[sym_id] = (coord[1:4], 0)
+    else:
+        print('Выделите хотя бы один лист.')
 
-    print(block_ids)
-    print('Количество блоков на листе:', len(block_ids))
-
+    #print(block_ids)
+    #print('Количество блоков на листе:', len(block_ids))
